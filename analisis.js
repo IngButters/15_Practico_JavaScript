adf3d97be8e23e18791830af576a5678f28f1165
Andres M <ibaguetolima@hotmail.com>
2022-03-17 11:23:27 -0500

    Finalizando el cuarto taller

--- analisis.js
+++ analisis.js
@@ -1,16 +1,4 @@
-// Creamos una lista que solo tenga los salarios
-const salariosCol = colombia.map(
-    function (personita){
-        return personita.salary;
-    }
-);
-
-const salariosColSorted = salariosCol.sort(
-    function (salaryA, salaryB) {
-        return salaryA - salaryB;
-    }
-);
-
+// Helpers o Utils -> Son funciones que no son parte de la logica de negocios pero se necesitan
 function esPar(numerito) {
     return (numerito % 2 === 0);
 }
@@ -32,6 +20,8 @@ function calcularMediaAritmetica(lista){
     
 }
 
+// Calcuadora de mediana
+
 
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2);
@@ -50,6 +40,38 @@ function medianaSalarios(lista) {
     }
 }
 
+ // Mediana General
+// Creamos una lista que solo tenga los salarios
+const salariosCol = colombia.map(
+    function (personita){
+        return personita.salary;
+    }
+);
+
+const salariosColSorted = salariosCol.sort(
+    function (salaryA, salaryB) {
+        return salaryA - salaryB;
+    }
+);
+
+
+
 console.log(
     medianaSalarios(salariosColSorted)
 )
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted)
+
+
+// Mediana del top 10%
+const spliceStart = (salariosColSorted.length * (90))/100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(spliceStart, spliceCount);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+})
